869291ed63c13dd2f2a2bc472e1ad402dcc934c8
mayke <mayke.jaspers@howest.be>
2025-11-08 19:37:26 +0100

    added config.ts file

---
+++ my-portfolio/src/content/config.ts
@@ -0,0 +1,25 @@
+import { defineCollection, z } from 'astro:content';
+
+const projects = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string().optional(),
+    date: z.date(),
+    tags: z.array(z.string()).optional(),
+    featured: z.boolean().default(false),
+  }),
+});
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { projects, blog };
